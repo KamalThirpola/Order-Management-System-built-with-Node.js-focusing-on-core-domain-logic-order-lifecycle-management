a9cf23238b84fbbd00f6544c225a2b14469a1382
KamalThirpola <ks2128152@gmail.com>
2026-01-17 21:38:50 +0530

    Implement courier management functions

--- couriers.js
+++ couriers.js
@@ -1,1 +1,31 @@
+// Add a courier
+function addCourier(name) {
+  const courier = {
+    id: data.couriers.length + 1,
+    name,
+    available: true
+  };
+
+  data.couriers.push(courier);
+  return courier;
+}
+
+// Get all couriers
+function getCouriers() {
+  return data.couriers;
+}
+
+// Assign courier
+function assignCourier() {
+  const courier = data.couriers.find(c => c.available);
+  if (!courier) return null;
+
+  courier.available = false;
+  return courier;
+}
+
+module.exports = {
+  addCourier,
+  getCouriers,
+
 
