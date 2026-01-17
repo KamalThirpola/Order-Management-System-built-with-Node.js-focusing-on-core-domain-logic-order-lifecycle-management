47409c6505b55ff6bb18c6f6ac8274824009cbda
KamalThirpola <ks2128152@gmail.com>
2026-01-17 21:33:14 +0530

    Implement create and update order functions

--- orders.js
+++ orders.js
@@ -1,1 +1,28 @@
+// orders.js
+
+const { orders } = require("./data");
+
+function createOrder(order) {
+  orders.push({
+    ...order,
+    status: "CREATED",
+  });
+  return order;
+}
+
+function updateOrderStatus(orderId, newStatus) {
+  const order = orders.find(o => o.id === orderId);
+
+  if (!order) {
+    throw new Error("Order not found");
+  }
+
+  order.status = newStatus;
+  return order;
+}
+
+module.exports = {
+  createOrder,
+  updateOrderStatus,
+};
 
